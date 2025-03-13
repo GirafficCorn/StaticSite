425798cb6680f686284739fdf39577b5375f2ba2
Brandon <bgtall96@gmail.com>
2025-03-12 19:28:16 -0700

    built recursive directory function

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from textnode import *
 from htmlnode import *
 from leafnode import *
 from parentnode import *
+import os
+import shutil
 
 def main():
-    Test_Object = TextNode("anchor text", TextType.LINK, "https://www.boot.dev")
-    print(Test_Object.__repr__())
+
+
+    pass
 
 
 main()
